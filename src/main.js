d50919bfc9b1bd637c56c66470f2158fc90ff68b
vanduc2514 <vanduc2514@gmail.com>
2026-04-08 05:09:26 +0700

    fix navigation does not work

--- src/main.js
+++ src/main.js
@@ -184,6 +184,18 @@ impressRoot.innerHTML = slides.map(renderSlide).join("");
 const api = window.impress();
 api.init();
 
+document.getElementById("nav-home").addEventListener("click", () => {
+  api.goto(slides[0].id);
+});
+
+document.getElementById("nav-prev").addEventListener("click", () => {
+  api.prev();
+});
+
+document.getElementById("nav-next").addEventListener("click", () => {
+  api.next();
+});
+
 document.addEventListener("click", (event) => {
   const target = event.target.closest("[data-goto]");
 
